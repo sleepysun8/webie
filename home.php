86675e7fb8b4b5285f34b66ae806ff2434326d8a
Theresia Ann <55128117+sleepysun8@users.noreply.github.com>
2019-09-10 14:24:40 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,165 @@
+<?php 
+include 'layoutcss.php';
+?>
+
+<body style="background-color: #c9c9c1">
+	<div class="position">
+		<div class="header1" >
+			<h1 align="center" style="font-size: 13pt; margin-top: 20px auto;" > Free Shipping! Untuk miminum transaksi Rp 300000  </h1>
+		</div>
+		<div class="header2">
+			<div>
+				<h1 style="font-size: 12pt; margin: 20px auto; margin-left: 40px;" >IDR</h1>	
+			</div>
+			<div align="center">
+				<img src="ccc.png" style="margin-left:60px;margin-top: 10px;" width="100px" height="35px" >
+			</div>
+			<div>
+				<ul class="nav" style=" text-decoration-color: #474749;">
+					<li><a href="">Account</a></li>
+					<li><a href="">Search</a></li>
+					<li><a href="">Cart</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="header3">
+			<ul class="nav" style="">
+				<li><a href="">Ancient chinese secret issue</a></li>
+				<li><a href="">New arrivals</a></li>
+				<li><a href="">Tshirt</a></li>
+				<li><a href="">Top</a></li>
+				<li><a href="">Bottom</a></li>
+				<li><a href="">Bag</a></li>
+				<li><a href="">Accessories</a></li>
+				<li><a href="">How to order</a></li>
+				<li><a href="">Blog post</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="slidepic1">
+		<img src="sl.jpg" style="height: 100%; width: 100%;">
+	</div>
+	<div class="slidepic2">
+		<img src="sl4.jpg" style="height: 100%; width: 100%;">
+	</div>
+	<div>
+		<h1 style="font-family: 'raleway'; font-size: 12pt; margin: 20px auto; color:  #474749; text-align: center;">
+			Daily Wear
+		</h1>
+	</div>
+	<div class="midtext">
+		<a href="" style="color:  #474749; text-decoration: none;">Tshirt</a>
+		<a href="" style="color:  #474749; text-decoration: none;">Pants</a>
+	</div>
+	<div class="gridhome">
+		<div>
+			<img src="AA.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="B.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="C.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="D.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+	</div>
+	<div class="gridhome">
+		<div>
+			<img src="AA.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="B.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="C.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="D.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+	</div>
+	<div style="text-align: center;">
+		<button type="button" class="btn btn-default" style="margin-top:30px; margin-bottom: 40px; background-color: black; color: white; font-family: 'raleway' ; font-size: 11pt; border-width:8px;">VIEW ALL PRODUCTS</button>
+		<hr>
+	</div>
+<hr>
+	<div style="text-align: center;">
+		<h1 style="margin-top: 50px; font-family: 'raleway'; margin-bottom: 20px;  font-size: 12pt; color:  #474749; text-align: center;">
+			Featured Colection
+		</h1>
+	</div>
+	<div class="midtext">
+		<a href="" style="color:  #474749; text-decoration: none;">SHIRT</a>
+		<a href="" style="color:  #474749; text-decoration: none;">SWEATER</a>
+	</div>
+	<div class="gridhome">
+		<div>
+			<img src="AA.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="B.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="C.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="D.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+	</div>
+	<div class="gridhome">
+		<div>
+			<img src="AA.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="B.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="C.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+		<div>
+			<img src="D.JPG" width="250px" height='250px' >
+			<br>
+			BASIC TSHIRT<br>Rp 125000
+		</div>
+	</div>
+	<div style="text-align: center;">
+		<button type="button" class="btn btn-default" style="margin-top:30px; margin-bottom: 40px; background-color: black; color: white; font-family: 'raleway' ; font-size: 11pt; border-width:8px;">VIEW ALL PRODUCTS</button>
+		<hr>
+		<div style="margin-bottom: 100px;">
+			
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ layoutcss.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NAME YOUR WEB HERE</title>
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Crimson+Pro&display=swap">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway&display=swap">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Didact+Gothic&display=swap">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Quicksand&display=swap">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">
+
+	/*--- css header menu---*/
+	.position{
+		position: fixed;
+		width: 100%;
+		}
+
+		.header1{
+			background-color: #0b2659;
+			color: white;
+			align-items: center;
+			align-content: center;
+			align-self: center;
+			/*margin: 5px auto;*/
+			height: 30px;
+			font-family: 'Raleway';
+			/*width: 100%;*/
+
+		}
+		.header2{
+			background-color: white;
+			text-decoration-color:  #474749;
+			/*color: #474749;*/
+			display: grid;
+			grid-template-columns: 1fr 3fr 2fr;
+			font-family: 'Raleway';
+			/*width: 100%;*/
+		}
+		.header3{
+			background-color:white;
+			color: #474749;
+			align-items: center;
+			align-content: center;
+			align-self: center;
+			margin: auto;
+			position: relative;
+			height: 50px;
+			font-family: 'Raleway';
+		}
+		/*--------DROPBUTTON----------*/	
+		.nav {
+			list-style: none;
+			float: none;
+			text-align: center;
+			color: #474749;
+			background: transparent;
+			/*margin-top: 10px;*/
+			margin-left: 40px;
+			font-family: 'Raleway';
+		}
+		.nav li {
+			display: block;
+			color:  #474749;
+			/*margin: 20px, auto;*/
+			position: relative;
+			float: center;
+			text-align: center;
+			background: transparent;
+		}
+		.nav li a {
+			display: grid;
+			color:  #474749;
+			/*width: 00px;*/
+			margin-left: 50px;
+			line-height: 30px;
+			text-decoration: none;
+			float: center;
+			text-align: center;
+		}
+
+		.slidepic1{
+			float: center;
+			background-color: grey;
+			height: 450px;
+
+			/*background-image: url(s1.jpg);*/
+			/*margin: 10px auto;*/
+		}
+
+		.slidepic2{
+			float: center;
+			background-color: #8f8f83;
+			height: 250px;
+			/*margin: 10px auto;*/
+		}
+		.midtext{
+			list-style: none;
+			color: #474749;
+			float: none;
+			text-align: center;
+			text-decoration: none;
+			background: transparent;
+			text-align: center;
+			font-family: 'Raleway';
+			font-size: 16pt;
+			margin: 9px auto;
+			margin-bottom: 30px;
+
+		}
+		.gridhome{
+			display: grid;
+			grid-template-columns: 1fr 1fr 1fr 1fr;
+			/*background:pink;*/
+			height: 350px;
+			margin: 10px auto;
+			font-size: 10pt;
+		}
+		.gridhome div{
+			/*border-style: dashed;*/
+			background-color: 
+			margin: 20px auto;
+			text-align: center;
+		}
+	</style>
+</head>
